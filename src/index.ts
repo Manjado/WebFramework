76be8cc20f8c31222a9e8c3efb72c3c2a77b8936
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-10-26 13:54:42 +0200

    add save,fetch and trigger methods

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
+// import { User } from './models/User';
+// const user = new User({ name: 'myname', age: 20 });
+
+// user.set({ name: 'newname', age: 9999 });
+
+// user.on('change', () => {
+//   console.log('change1');
+// });
+// user.on('change', () => {
+//   console.log('change2');
+// });
+// user.on('save', () => {
+//   console.log('save');
+// });
+
+// user.trigger('xxsxa');
+
+// import axios from 'axios';
+
+// axios.get('http://localhost:3000/users/1');
+
 import { User } from './models/User';
-const user = new User({ name: 'myname', age: 20 });
 
-user.set({ name: 'newname', age: 9999 });
+const user = new User({ id: 1 });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.set({ name: 'Manjado', age: 33 });
+
+user.save();
