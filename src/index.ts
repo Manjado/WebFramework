94ca2d4fe872fb4b6ade161cb1d5fea51acaa6f8
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-10-26 15:11:27 +0200

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,8 @@ import { User } from './models/User';
 
 const user = new User({ id: 1 });
 
-user.set({ name: 'Manjado', age: 33 });
+user.events.on('change', () => {
+  console.log('Change!');
+});
 
-user.save();
+user.events.trigger('change');
